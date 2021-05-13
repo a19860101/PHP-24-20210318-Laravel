d82de4469b51ac63d86c70cbb37e29d203b763a9
Zac <a19860101@gmail.com>
2021-05-13 20:01:11 +0800

    20210513

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,17 +1,27 @@
 @extends('template.master')
 @section('main')
-    <h1>Post Create</h1>
-    <form action="{{route('post.store')}}" method="post">
-        @csrf
-        <div>
-            <label for="">文章標題</label>
-            <input type="text" name="title">
+    <div class="container">
+        <div class="row  justify-content-center">
+            <div class="col-lg-8 col-10">
+                <h1>建立文章</h1>
+                <hr>
+            </div>
+            <div class="col-lg-8 col-10">
+                <form action="{{route('post.store')}}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="" class="form-label">文章標題</label>
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">內文</label>
+                        <textarea name="content" id=""  rows="10" class="form-control"></textarea>
+                    </div>
+                    <input type="submit" value="新增" class="btn btn-primary">
+                    <input type="button" value="取消" onclick="history.back()" class="btn btn-danger">
+                </form>
+            </div>
         </div>
-        <div>
-            <label for="">內文</label>
-            <textarea name="content" id="" cols="30" rows="10"></textarea>
-        </div>
-        <input type="submit" value="新增">
-        <input type="button" value="取消" onclick="history.back()">
-    </form>
+    </div>
+    
 @endsection
