99a5cce2a6c2dfe11d0be4069349a15547c681f3
Zac <a19860101@gmail.com>
2021-05-13 20:10:43 +0800

    20210513

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -15,7 +15,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">內文</label>
-                        <textarea name="content" id=""  rows="10" class="form-control"></textarea>
+                        <textarea name="content" id="content"  rows="20" class="form-control"></textarea>
                     </div>
                     <input type="submit" value="新增" class="btn btn-primary">
                     <input type="button" value="取消" onclick="history.back()" class="btn btn-danger">
@@ -23,5 +23,39 @@
             </div>
         </div>
     </div>
-    
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: '#content',
+            language: 'zh_TW',
+            menubar: false,
+            plugins: 'image code link imagetools lists',
+            toolbar: 'undo redo | image code link| styleselect bullist numlist| bold italic forecolor underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent',
+            // images_upload_url: 'postAcceptor.php',
+            // automatic_uploads: true,
+            // image_title: true,
+            // file_picker_types: 'image',
+            // file_picker_callback: function (cb, value, meta) {
+            // var input = document.createElement('input');
+            // input.setAttribute('type', 'file');
+            // input.setAttribute('accept', 'image/*');
+            // input.onchange = function () {
+            // var file = this.files[0];
+            // var reader = new FileReader();
+            // reader.onload = function () {
+            //     var id = 'blobid' + (new Date()).getTime();
+            //     var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+            //     var base64 = reader.result.split(',')[1];
+            //     var blobInfo = blobCache.create(id, file, base64);
+            //     blobCache.add(blobInfo);
+            //     cb(blobInfo.blobUri(), { title: file.name });
+            // };
+            // reader.readAsDataURL(file);
+            // };
+
+            // input.click();
+        // }
+    });
+    </script>
+
 @endsection
